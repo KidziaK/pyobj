dacfc1dc653e676c670a915e55e2387277bcb923
Anna Kozłowska <anna.kozlowska@skyengine.ai>
2024-10-01 16:52:45 +0200

    Added changes according to PR#1

--- pyobj/main.py
+++ pyobj/main.py
@@ -1,9 +1,7 @@
-import numpy as np
-
 from typing import Union
 from pathlib import Path
 
-from pyobj.c_lib import fast_obj_read, fast_obj_write, fastObjMesh
+from pyobj.c_lib import fast_obj_read, fast_obj_write
 from pyobj.mesh import Mesh
 
 def read(path: Union[Path, str]) -> Mesh:
@@ -11,6 +9,13 @@ def read(path: Union[Path, str]) -> Mesh:
     return Mesh(mesh_p)
 
 
-def write(mesh: Mesh, path: Union[Path, str]) -> int:
+def write(mesh: Mesh, path: Union[Path, str]) -> None:
     status = fast_obj_write(mesh.pointer, path)
-    return status
+    if status == 5:
+        raise IOError(f"While opening file ({path}) for writing, an exception occured.") 
+    elif status != 0:
+        raise RuntimeError("Unknown error occurred during OBJ export.")
+    
+
+class IOError(Exception):
+    pass
--- src/fast_obj/fast_obj_export.h
+++ src/fast_obj/fast_obj_export.h
@@ -7,38 +7,44 @@ int fast_obj_write(fastObjMesh* mesh, const char* path)
 
     if (!(fout = fopen(path, "w")))
     {
-        printf("Error!");
-        return 0;
+        printf("Error - couldn't open file for writing (path: %s)", path);
+        return 5;
     }
-    int getArrayLength = sizeof(mesh->positions) / sizeof(mesh->positions[0]);
-    printf("%d; %d!", mesh->position_count, getArrayLength);    
+
+    // Write all positions
     for (unsigned int pi = 1; pi < mesh->position_count; ++pi)
     {
         fprintf(fout, "v %f %f %f\n", 
                         mesh->positions[pi*3], mesh->positions[pi*3+1], mesh->positions[pi*3+2]);   
     }
 
+    // Write all tex coords
     for (unsigned int ti = 1; ti < mesh->texcoord_count; ++ti)
     {
         fprintf(fout, "vt %f %f\n", 
                         mesh->texcoords[ti*2], mesh->texcoords[ti*2+1]);   
     }
 
+    // Write all normals
     for (unsigned int ni = 1; ni < mesh->normal_count; ++ni)
     {
         fprintf(fout, "vn %f %f %f\n", 
                         mesh->normals[ni*3], mesh->normals[ni*3+1], mesh->normals[ni*3+2]);   
     }
 
+    // For every object in the structure...
     for (unsigned int oi = 0; oi < mesh->object_count; ++oi)
     {
         fprintf(fout, "o %s\n", mesh->objects[oi].name);
         unsigned int ii = mesh->objects[oi].index_offset;
+        // ...write all faces
         for (unsigned int fi = 0; fi < mesh->objects[oi].face_count; ++fi)
         {
             fprintf(fout, "f ");
+            // We have to go through all vertices to write them in file
             for (unsigned int vi = 0; vi < mesh->face_vertices[fi + mesh->objects[oi].face_offset]; ++vi)
             {
+                // If t == 0, then tex coords are undefined and we can skip them
                 if(mesh->indices[ii].t == 0)
                     fprintf(fout, "%d//%d ", mesh->indices[ii].p, mesh->indices[ii].n);
                 else
@@ -51,4 +57,5 @@ int fast_obj_write(fastObjMesh* mesh, const char* path)
     }
 
     fclose(fout);
+    return 0;
 }
